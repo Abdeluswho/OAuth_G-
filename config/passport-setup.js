eef6b1dba2c8c625f6b09455dd29929c61ba671c
A.Sofiane Guerfi <asofianeguerfi@ASofianes-MacBook-Pro.local>
2018-04-09 23:25:44 -0700

    passport_OAuthtest

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -3,6 +3,16 @@ const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
 const User = require('../models/user-model');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user)
+    })
+    
+})
 
 passport.use(
     new GoogleStrategy({
@@ -18,6 +28,7 @@ passport.use(
         }).then((currentUser)=>{
             if(currentUser){
                 console.log('user is: ', currentUser)
+                done(null ,currentUser);
             }else {
 
                 new User({
@@ -25,6 +36,7 @@ passport.use(
                     googleid: profile.id
                 }).save().then((newUser) => {
                     console.log("new user created: ", newUser);
+                    done(null, newUser);
                 })
 
             }
